354c1b538fb0b6d6fefeb8b3f0707bbecfe039b6
Houssam Haraf <houssamharaf2@gmail.com>
2024-01-14 22:01:59 +0100

    v2

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from houssam   spring boot application :)";
+        return "Hello from houssam haraf  spring boot application :)";
     }
 
     @GetMapping("/user")
